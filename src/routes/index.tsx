463438e31cab89c61f3e9b53f94e0841e4897f95
Renato Moschetta <renatorock3@hotmail.com>
2020-09-21 16:12:49 -0300

    router crashed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { Switch, BrowserRouter } from "react-router-dom";
+import { Switch, HashRouter } from "react-router-dom";
 
 import Route from "./Routes";
 
 import SignIn from "../pages/SignIn";
-// import SignUp from "../pages/SignUp";
-// import Profile from "../pages/Profile";
-// import Detail from "../pages/Detail";
-// import ForgotPassowrd from "../pages/ForgotPassword";
-// import ResetPassword from "../pages/ResetPassword";
-// import Dashboard from "../pages/Dashboard";
-// import NewProduct from "../pages/NewProduct";
-// import Shop from "../pages/Shop/Home";
-// import Purchase from "../pages/Shop/Purchase";
+import SignUp from "../pages/SignUp";
+import Profile from "../pages/Profile";
+import Detail from "../pages/Detail";
+import ForgotPassowrd from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
+import Dashboard from "../pages/Dashboard";
+import NewProduct from "../pages/NewProduct";
+import Shop from "../pages/Shop/Home";
+import Purchase from "../pages/Shop/Purchase";
 
-// import NotFound from "../components/NotFound";
+import NotFound from "../components/NotFound";
 
 const Routes: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" exact component={SignIn} />
 
-        {/* <Route path="/signup" component={SignUp} />
+        <Route path="/signup" component={SignUp} />
         <Route path="/forgot-password" component={ForgotPassowrd} />
         <Route path="/reset-password" component={ResetPassword} />
 
@@ -34,9 +34,9 @@ const Routes: React.FC = () => {
         <Route path="/product/detail" component={Detail} isPrivate />
         <Route path="/product/new" component={NewProduct} isPrivate />
 
-        <Route path="*" component={NotFound} /> */}
+        <Route path="*" component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
