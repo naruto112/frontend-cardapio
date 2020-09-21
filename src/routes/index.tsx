75afedfa2f741e7110fe03e1d53404bd264644ad
Renato Moschetta <renatorock3@hotmail.com>
2020-09-21 15:42:53 -0300

    up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,40 +1,42 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, BrowserRouter } from "react-router-dom";
 
 import Route from "./Routes";
 
 import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
-import Profile from "../pages/Profile";
-import Detail from "../pages/Detail";
-import ForgotPassowrd from "../pages/ForgotPassword";
-import ResetPassword from "../pages/ResetPassword";
-import Dashboard from "../pages/Dashboard";
-import NewProduct from "../pages/NewProduct";
-import Shop from "../pages/Shop/Home";
-import Purchase from "../pages/Shop/Purchase";
+// import SignUp from "../pages/SignUp";
+// import Profile from "../pages/Profile";
+// import Detail from "../pages/Detail";
+// import ForgotPassowrd from "../pages/ForgotPassword";
+// import ResetPassword from "../pages/ResetPassword";
+// import Dashboard from "../pages/Dashboard";
+// import NewProduct from "../pages/NewProduct";
+// import Shop from "../pages/Shop/Home";
+// import Purchase from "../pages/Shop/Purchase";
 
-import NotFound from "../components/NotFound";
+// import NotFound from "../components/NotFound";
 
 const Routes: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={SignIn} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={SignIn} />
 
-      <Route path="/signup" component={SignUp} />
-      <Route path="/forgot-password" component={ForgotPassowrd} />
-      <Route path="/reset-password" component={ResetPassword} />
+        {/* <Route path="/signup" component={SignUp} />
+        <Route path="/forgot-password" component={ForgotPassowrd} />
+        <Route path="/reset-password" component={ResetPassword} />
 
-      <Route path="/cardapio/:shop" exact component={Shop} isPath />
-      <Route path="/cardapio/:shop/order" component={Purchase} isPath />
+        <Route path="/cardapio/:shop" exact component={Shop} isPath />
+        <Route path="/cardapio/:shop/order" component={Purchase} isPath />
 
-      <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/profile" component={Profile} isPrivate />
-      <Route path="/product/detail" component={Detail} isPrivate />
-      <Route path="/product/new" component={NewProduct} isPrivate />
+        <Route path="/dashboard" component={Dashboard} isPrivate />
+        <Route path="/profile" component={Profile} isPrivate />
+        <Route path="/product/detail" component={Detail} isPrivate />
+        <Route path="/product/new" component={NewProduct} isPrivate />
 
-      <Route path="*" component={NotFound} />
-    </Switch>
+        <Route path="*" component={NotFound} /> */}
+      </Switch>
+    </BrowserRouter>
   );
 };
 
